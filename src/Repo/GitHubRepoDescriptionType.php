2d544473ebdd83746a08cff4218b2c08c0ffa1ee
Miro Svrtan <miro@mirosvrtan.me>
2017-06-14 21:08:31 +0200

    Source code imported from app

---
+++ src/Repo/GitHubRepoDescriptionType.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devboard\GitHub\DoctrineTypes\Repo;
+
+use Devboard\GitHub\Repo\GitHubRepoDescription;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class GitHubRepoDescriptionType extends Type
+{
+    /** @SuppressWarnings("PHPMD.UnusedFormalParameter") */
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        return $platform->getVarcharTypeDeclarationSQL(['length' => '3000']);
+    }
+
+    /** @SuppressWarnings("PHPMD.UnusedFormalParameter") */
+    public function convertToPhpValue($value, AbstractPlatform $platform)
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        return new GitHubRepoDescription($value);
+    }
+
+    /** @SuppressWarnings("PHPMD.UnusedFormalParameter") */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        return (string) $value;
+    }
+
+    /** @SuppressWarnings("PHPMD.UnusedFormalParameter") */
+    public function requiresSqlCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
+    }
+
+    public function getName(): string
+    {
+        return 'GitHubRepoDescription';
+    }
+}
